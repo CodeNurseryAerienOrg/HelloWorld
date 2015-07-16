362ed94ceac3122f34ee4235cba67b306dfdeada
Jean Lestang <jelestang@airfrance.fr>
2015-07-16 11:31:38 +0200

    A working example from D3js.org

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: 'Joe' });
-});
+router
+    .get('/', function(req, res, next) {
+        res.render('index', { title: 'Express', user: 'Joe'  });
+    })
+    .get('/d3', function(req, res, next) {
+        res.render('d3', { title: 'Essai D3'});
+    });
 
 module.exports = router;
