e10d47674c5302be3b1700a54d87c8bc0b20a4b9
Jean Lestang <jelestang@airfrance.fr>
2015-07-22 13:53:27 +0200

    Un peu plus loin avec D3.js

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ router
     .get('/', function(req, res, next) {
         res.render('index', { title: 'Express', user: 'Joe'  });
     })
-    .get('/d3', function(req, res, next) {
-        res.render('d3', { title: 'Essai D3'});
+    .get('/d3/:id', function(req, res, next) {
+        console.log("get d3: " +  req.params.id);
+        res.render('d3_' + req.params.id, { title: 'Essai D3' +  req.params.id});
     });
 
 module.exports = router;
